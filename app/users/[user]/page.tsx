f57640b3185ab6553d00422d4f98d531c14adf96
Grady Wasil <arrangedgodly@Gradys-MacBook-Air.local>
2023-07-06 15:58:20 -0600

    refactor: more edits but I am having a hard time with firebase and fixing errors on next

--- app/users/[user]/page.tsx
+++ app/users/[user]/page.tsx
@@ -2,7 +2,8 @@
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/lib/firebase";
-import { signOutUser } from "@/lib/firebase";
+import { signOutUser, getUser } from "@/lib/firebase";
+import { useEffect, useState } from "react";
 
 interface Params {
   params: {
@@ -10,12 +11,34 @@ interface Params {
   };
 }
 
+interface UserData {
+  name: string;
+  email: string;
+  photoURL: string;
+  uid: string;
+}
+
 export default function Page({ params }: Params) {
   const [user, loading, error] = useAuthState(auth);
+  const [userData, setUserData] = useState<UserData | null>(null);
   const currentUser = user?.uid === params.user;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const data = await getUser(params.user);
+      setUserData(data);
+    }
+
+    fetchUser();
+  }, [params.user]);
 
   return (
     <div>
+      {userData !== null && (
+        <>
+          <h1>{userData.name}</h1>
+        </>
+      )}
       {currentUser && (
         <button onClick={signOutUser}>Sign Out</button>
       )}
--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection, setDoc, getDoc, doc } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import {
   getAuth,
@@ -26,10 +26,35 @@ export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
 export const googleAuthProvider = new GoogleAuthProvider();
+const usersCollection = collection(db, "users");
+
+const createUser = async (user: any) => {
+  await setDoc(doc(usersCollection, user.uid), user);
+};
+
+export const getUser = async (uid: string) => {
+  const userDoc = doc(db, "users", uid);
+  const docSnap = await getDoc(userDoc);
+
+  if (docSnap.exists()) {
+    return docSnap.data();
+  } else {
+    return null;
+  }
+};
 
 export const signInWithGoogle = async () => {
   await signInWithPopup(auth, googleAuthProvider);
   if (auth.currentUser) {
+    const user = await getUser(auth.currentUser.uid);
+    if (!user) {
+      createUser({
+        uid: auth.currentUser.uid,
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email,
+        photoUrl: auth.currentUser.photoURL,
+      });
+    }
     redirect("/");
   }
 };
@@ -37,6 +62,15 @@ export const signInWithGoogle = async () => {
 export const signUpWithEmail = async (email: string, password: string) => {
   await createUserWithEmailAndPassword(auth, email, password);
   if (auth.currentUser) {
+    const user = await getUser(auth.currentUser.uid);
+    if (!user) {
+      createUser({
+        uid: auth.currentUser.uid,
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email,
+        photoUrl: auth.currentUser.photoURL,
+      });
+    }
     redirect("/");
   }
 };
@@ -44,6 +78,15 @@ export const signUpWithEmail = async (email: string, password: string) => {
 export const signInWithEmail = async (email: string, password: string) => {
   await signInWithEmailAndPassword(auth, email, password);
   if (auth.currentUser) {
+    const user = await getUser(auth.currentUser.uid);
+    if (!user) {
+      createUser({
+        uid: auth.currentUser.uid,
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email,
+        photoUrl: auth.currentUser.photoURL,
+      });
+    }
     redirect("/");
   }
 };
